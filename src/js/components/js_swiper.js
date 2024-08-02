8160477f0c33a3255fc7689a73dafa5fbea7e180
Yaroslava Sabitova <slava.sabitova@yandex.ru>
2024-08-02 21:54:01 +0300

    import swiper locally

--- src/js/components/js_swiper.js
+++ src/js/components/js_swiper.js
@@ -1,15 +1,13 @@
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+// import Swiper from 'swiper';
+// import { Navigation, Pagination } from 'swiper/modules';
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
 
 export default function () {
     if (window.innerWidth <= 929) {
-        console.log(1);
-
         const swiper = new Swiper('.js_swiper_advantages', {
-            modules: [Navigation, Pagination],
+            // modules: [Navigation, Pagination],
             direction: 'horizontal',
             slidesPerView: 'auto',
             centeredSlides: true,
@@ -23,20 +21,6 @@ export default function () {
                 nextEl: '.swiper-button-next',
                 prevEl: '.swiper-button-prev'
             }
-            // breakpoints: {
-            //     929: {
-            //         slidesPerView: 2,
-            //         spaceBetween: 25
-            //     },
-            //     610: {
-            //         slidesPerView: 2,
-            //         spaceBetween: 25
-            //     },
-            //     300: {
-            //         slidesPerView: 1,
-            //         spaceBetween: 25
-            //     }
-            // }
         });
     }
 }
